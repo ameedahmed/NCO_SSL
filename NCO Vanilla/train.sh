2c5531b2106d9c8e08fc2e636a334449a732b08f
Ameed_Ahmed <46124638+ameedahmed@users.noreply.github.com>
2025-09-14 09:44:01 +0200

    Add files via upload
    This folder contains the vanilla training strategy of the LEHD Model.

---
+++ NCO Vanilla/train.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=V.R.T
+#SBATCH --output=test%j.log
+#SBATCH --error=test%j.err
+#SBATCH --mail-user=ahmed003@ismll.de
+#SBATCH --partition=STUDL
+#SBATCH --gres=gpu:1
+
+
+cd /home/ahmed003/NCO_code/single_objective/LEHD/TSP/train.py # navigate to the directory if necessary
+
+
+srun /home/ahmed003/miniconda3/envs/srpenv/bin/python3.10 train.py     # run the script
